ad8d34d797c2c9e6cc6b15d1f44945eb298aa935
Estuar Horna <bhornaf93@gmail.com>
2022-07-21 21:44:21 -0500

    Router y Multer

--- Contenedor.js
+++ Contenedor.js
@@ -6,66 +6,61 @@ class Contenedor {
         this.data = [];
     }
 
-    readFile() {
-        const data = fs.readFileSync(this.fileName, "utf-8");
+    readFile = async () => {
+        const data = await fs.promises.readFile(this.fileName, "utf8");
         this.data = JSON.parse(data);
-    }
-
-    save(product) {
-        const maxId = this.data.reduce((max, product) => {
-            return product.id > max ? product.id : max;
-        }, 0);
-
-        const id = maxId + 1;
-        const newProduct = {
-            ...product,
-            id,
-        };
-        const length = this.data.length;
-
-        this.data[length] = newProduct;
-        fs.writeFileSync(this.fileName, JSON.stringify(this.data));
-        return id;
-    }
-    getById(id) {
-        const product = this.data.find((product) => product.id === id);
-        return product;
-    }
-
-    getAll() {
         return this.data;
-    }
+    };
 
-    deleteById(id) {
-        const index = this.data.findIndex((product) => product.id === id);
-        this.data.splice(index, 1);
-        fs.writeFileSync(this.fileName, JSON.stringify(this.data));
-    }
+    getAll = async () => {
+        const data = await this.readFile();
+        return this.data;
+    };
 
-    deleteAll() {
+    getRandom = async () => {
+        const data = await this.getAll();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        return data[randomIndex];
+    };
+
+    getById = async (id) => {
+        const data = await this.getAll();
+        const producto = data.find((producto) => producto.id === id);
+        return producto;
+    };
+
+    save = async (producto) => {
+        const data = await this.getAll();
+        const maxId = data.reduce((max, producto) => {
+            return producto.id > max ? producto.id : max;
+        }, 0);
+        const id = maxId + 1;
+        const newProducto = { ...producto, id };
+
+        const maxLength = data.length;
+        this.data[maxLength] = newProducto;
+        await fs.promises.writeFile(this.fileName, JSON.stringify(this.data));
+        return id;
+    };
+
+    deleteById = async (id) => {
+        const data = await this.getAll();
+        const index = data.findIndex((producto) => producto.id === id);
+        data.splice(index, 1);
+        await fs.promises.writeFile(this.fileName, JSON.stringify(data));
+    };
+
+    updateById = async (id, producto) => {
+        const data = await this.getAll();
+        const index = data.findIndex((producto) => producto.id === id);
+        data[index] = producto;
+        await fs.promises.writeFile(this.fileName, JSON.stringify(data));
+    };
+
+    deleteAll = async () => {
         this.data = [];
-        fs.writeFileSync(this.fileName, JSON.stringify(this.data));
-    }
-
-    getRandom() {
-        const index = Math.floor(Math.random() * this.data.length);
-        return this.data[index];
-    }
+        await fs.promises.writeFile(this.fileName, JSON.stringify([]));
+    };
 }
 
 exports = module.exports = Contenedor;
-
-// const contenedor = new Contenedor("productos.txt");
-// contenedor.readFile();
-// console.log(contenedor.getAll());
-
-// const product = {
-//     title: "Laptop",
-//     price: 1000,
-//     thumbnail:
-//         "https://images-na.ssl-images-amazon.com/images/I/61-YZ-X-8L._AC_SL1500_.jpg",
-// };
-
-// console.log(contenedor.save(product));
-// console.log(contenedor.getById(1));
-// contenedor.deleteById(2);
--- server.js
+++ server.js
@@ -1,23 +1,94 @@
 const express = require("express");
+const { Router } = express;
+
 const contenedor = require("./Contenedor");
 const productos = new contenedor("productos.txt");
 
-productos.readFile();
-
 const app = express();
+const router = Router();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// router.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
+
+router.get("/productos", (req, res) => {
+    productos
+        .getAll()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
-app.get("/productos", (req, res) => {
-    res.send(productos.getAll());
+router.get("/producto/:id", (req, res) => {
+    console.log(req.params.id);
+    const id = parseInt(req.params.id);
+    productos
+        .getById(id)
+        .then((data) => {
+            res.send(data ? data : "Producto no encontrado");
+            // if (data) {
+            //     res.send(data);
+            // } else {
+            //     res.send("No existe el producto");
+            // }
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
-app.get("/productoRandom", (req, res) => {
-    const product = productos.getRandom();
-    res.send(product);
+router.get("/productoRandom", (req, res) => {
+    productos
+        .getRandom()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
+
+router.post("/productos", (req, res) => {
+    productos
+        .save(req.body)
+        .then((data) => {
+            res.send("El id es: " + data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+router.put("/productos/:id", (req, res) => {
+    productos
+        .updateById(req.params.id, req.body)
+        .then((data) => {
+            res.send("El producto se actualizo correctamente");
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+router.delete("/productos/:id", (req, res) => {
+    productos
+        .deleteById(req.params.id)
+        .then((data) => {
+            res.send("El producto se elimino correctamente");
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+app.use("/api", router);
 
 const PORT = process.env.PORT || 8080;
 
